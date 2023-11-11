cec358c384aa93d6081d9914f1dcba12b5b66e27
Sonia364 <c0872364@mylambton.ca>
2023-11-11 13:46:57 -0500

    Added task progress bar

--- components/Common/TaskDetail.js
+++ components/Common/TaskDetail.js
@@ -1,16 +1,37 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 const TaskDetail = ({ route }) => {
-  // const { taskName, taskDescription, createdDate, assignedMember, status } = route.params;
+  let progress = 0;
+  let progressColor = '';
+  let status = 'In Progress';
+  if (status === 'Completed') {
+    progress = 100;
+    progressColor = '#4CAF50';
+  } else if (status === 'In Progress') {
+    progress = 50;
+    progressColor = '#5D5FDE';
+  }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.taskName}>Task 1</Text>
-      <Text style={styles.taskDescription}>ply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letrase</Text>
-      <Text style={styles.createdDate}>Created Date: 09/11/2023</Text>
-      <Text style={styles.assignedMember}>Assigned Member: Sonia</Text>
-      <Text style={styles.status}>Status: In Progress</Text>
+      <View style={styles.progressBarContainer}>
+        <LinearGradient
+          colors={['#fff', progressColor]} // Add white gradient on both sides
+          style={[styles.progressBar, { width: `${progress}%` }]}
+        />
+      </View>
+      <View style={styles.taskWrap}>
+        <Text style={styles.taskName}>Task 1</Text>
+        <Text style={styles.taskDescription}>ply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letrase</Text>
+        <View style={styles.contentWrap}>
+        <Text style={styles.label}>Created Date:</Text><Text style={styles.value}>09/11/2023</Text>
+        </View>
+        <View style={styles.contentWrap}>
+          <Text style={styles.label}>Assigned Member:</Text><Text style={styles.value}>Sonia</Text>
+        </View>
+      </View>
     </View>
   );
 };
@@ -22,6 +43,12 @@ const styles = StyleSheet.create({
     padding: 20,
     borderRadius: 4,
   },
+  taskWrap:{
+    marginTop: 20,
+    backgroundColor: '#f9f9f9',
+    minHeight: 200,
+    padding: 20
+  },
   taskName: {
     fontSize: 18,
     fontWeight: 'bold',
@@ -32,21 +59,36 @@ const styles = StyleSheet.create({
     color: 'gray',
     fontSize: 14,
     marginBottom: 10,
+    textAlign: 'justify',
   },
-  createdDate: {
-    fontSize: 12,
-    color: 'grey',
-    marginBottom: 10,
+  contentWrap: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginVertical: 5,
   },
-  assignedMember: {
-    fontSize: 12,
-    color: 'grey',
-    marginBottom: 10,
+  label: {
+    fontWeight: 'bold',
+    color: '#000'
+  },
+  value: {
+    color: 'grey'
   },
   status: {
     fontSize: 12,
     color: 'grey',
     marginBottom: 10,
+  },
+  progressBarContainer: {
+    height: 8,
+    backgroundColor: '#ccc',
+    borderRadius: 4,
+    marginVertical: 8,
+    overflow: 'hidden',
+  },
+  progressBar: {
+    height: '100%',
+    borderRadius: 4,
   },
 });
 
